7e6b94e577c614b43b6db4e70d6578cf40050891
Tomasz Reizer <t_reizer@yahoo.com>
2018-12-27 22:04:59 +0100

    Changed script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    fetch(quoteUrl, { cache: "no-store" })
+    fetch(prefix + quoteUrl, { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
@@ -21,4 +21,22 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+    getQuote();
+} else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    document.querySelector('.quote').innerText = quoteText;
+    document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+    document.querySelector('.tweet').setAttribute('href', tweet);
 }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
+});
